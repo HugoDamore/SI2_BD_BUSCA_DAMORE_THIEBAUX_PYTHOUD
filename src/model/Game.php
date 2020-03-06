9ab355488c53a1de77fd6a04ba367e0ab0d86a69
Laury Thiebaux <laury.thiebaux1@etu.univ-lorraine.fr>
2020-03-06 09:27:23 +0100

    q1 finieé

--- src/model/Game.php
+++ src/model/Game.php
@@ -12,6 +12,6 @@ class Game extends Model
 
     public function Personnages() {
         return $this->belongsToMany('games\model\Character',
-            'games\model\Game2character', 'character_id', 'game_id');
+             'game2character', 'character_id', 'game_id');
     }
 }
