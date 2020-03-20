5b19d4790d87f05674749b15b75e79d76d3a0dde
Laury Thiebaux <laury.thiebaux1@etu.univ-lorraine.fr>
2020-03-20 10:05:11 +0100

    Partie 1

--- scripts/script3.php
+++ scripts/script3.php
@@ -6,21 +6,21 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use games\model\Game;
 use Illuminate\Database\Capsule\Manager as DB;
 
-
 \games\AppConf::addDbConf('../config/conf.ini');
-
-echo "<h2>Mesures des temps d'execution</h2>";
 
 $control = new \games\control\td3();
 
-//$control->q1();
+echo "<h2>Mesures des temps d'execution</h2>";
+
+$control->q1();
 $control->q2();
 $control->q3();
 $control->q4();
 
-
 echo "<h2>Questions sur les index</h2>";
-
+$control->q5_1();
+$control->q5_2();
+$control->q5_3();
 
 
 
--- src/control/td3.php
+++ src/control/td3.php
@@ -4,6 +4,8 @@
 namespace games\control;
 
 
+use games\model\Game;
+
 class td3
 {
 
@@ -24,8 +26,12 @@ class td3
         echo "<h3>q2</h3>";
         $start = microtime(true);
         $games = Game::where('name','like','%Mario%')->get();
+        foreach ($games as $game){
+            echo $game->name . '<br>';
+        }
         $end = microtime(true);
         $tmp = $end - $start;
+        echo "<br>";
         echo "Time : ".$tmp;
     }
 
@@ -58,6 +64,56 @@ class td3
         echo "<br>";
         echo "Time : ".$tmp;
     }
+
+    public function q5_1(){
+        echo "<h3>q5 - première valeur</h3>";
+        $start = microtime(true);
+        $games = Game::where('name', 'like', 'Mario%')->get();
+        foreach ($games as $game){
+            echo $game->name . '<br>';
+        }
+        $end = microtime(true);
+        $tmp = $end - $start;
+        echo "<br>";
+        echo "Time : ".$tmp;
+    }
+
+    public function q5_2(){
+        echo "<h3>q5 - deuxième valeur</h3>";
+        $start = microtime(true);
+        $games = Game::where('name', 'like', 'Sonic%')->get();
+        foreach ($games as $game){
+            echo $game->name . '<br>';
+        }
+        $end = microtime(true);
+        $tmp = $end - $start;
+        echo "<br>";
+        echo "Time : ".$tmp;
+    }
+
+    public function q5_3(){
+        echo "<h3>q5 - troisième valeur</h3>";
+        $start = microtime(true);
+        $games = Game::where('name', 'like', 'Run%')->get();
+        foreach ($games as $game){
+            echo $game->name . '<br>';
+        }
+        $end = microtime(true);
+        $tmp = $end - $start;
+        echo "<br>";
+        echo "Time : ".$tmp;
+    }
+
+    /**
+     * créer un index sur la colonne 'name' de la table 'game'
+     * CREATE INDEX `index_game_name` ON `game` (`name`)
+     */
+
+    /**
+     * Test des questions précédentes avec des nouvelles valeurs
+     *
+     * On constate que les requêtes sont beaucoup plus rapides avec un index.
+     */
 
 
 }
