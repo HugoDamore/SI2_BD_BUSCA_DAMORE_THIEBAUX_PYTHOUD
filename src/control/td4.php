5e0561111b08e32156e9d07f5f209e6ebdd59136
Laury Thiebaux <laury.thiebaux1@etu.univ-lorraine.fr>
2020-03-23 16:59:27 +0100

    problèmes résolus

--- src/control/td4.php
+++ src/control/td4.php
@@ -6,9 +6,9 @@ namespace games\control;
 
 use games\model\Game;
 use games\model\Company;
+use games\model\Utilisateur;
 use Illuminate\Database\Capsule\Manager as DB;
-
-use Games\Utilisateur;
+use Faker;
 
 class td4 {
 
@@ -17,12 +17,12 @@ class td4 {
         $faker = Faker\Factory::create('fr_FR');
 
         for ($i = 0; $i < 5; $i++) {
-            $user = new Utilisateur;
+            $user = new Utilisateur();
             $user->nom = $faker->lastName;
-            $user->prenoms = $faker->firstName;
+            $user->prenom = $faker->firstName;
             $user->email = $faker->unique()->email;
-            $user->date_naissance = $faker->dateTimeThisCentury->format('Y-m-d');
-            $user->telephone = $faker->phoneNumber;
+            $user->date_naiss = $faker->dateTimeThisCentury->format('Y-m-d');
+            $user->tel = $faker->phoneNumber;
             $user->save();
         }
     }
