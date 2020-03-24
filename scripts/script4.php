5d65dad90120a5360adf9fa6a14ae922ffaa96da
Hugo D'Amore <>
2020-03-24 11:52:24 +0100

    aaa

--- scripts/script4.php
+++ scripts/script4.php
@@ -11,8 +11,5 @@ $control->creationUtilisateurs();
 $control->creationCommentaires();
 
 //$control->q1();
-<<<<<<< HEAD
 //$control->q2();
-=======
-$control->q2();
->>>>>>> f035c61654dc27276a9d070787fb32992e048601
+
