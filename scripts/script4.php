0f113084ec0a2bf2e7a0e709522e54f29187bd5c
Laury Thiebaux <laury.thiebaux1@etu.univ-lorraine.fr>
2020-03-24 10:15:08 +0100

    scipt 4

--- scripts/script4.php
+++ scripts/script4.php
@@ -7,16 +7,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $control = new \games\control\td4();
 
-$control->creationUtilisateurs();
-$control->creationCommentaires();
+//$control->creationUtilisateurs();
+//$control->creationCommentaires();
 
-<<<<<<< HEAD
+
 $control->q1();
-=======
-//$control->q1();
-<<<<<<< HEAD
-//$control->q2();
-=======
->>>>>>> 6bc99eccbd6ca1388e109cfc2a3c3d366a8a71cf
 $control->q2();
->>>>>>> f035c61654dc27276a9d070787fb32992e048601
